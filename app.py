345d4a244ee959a926abcba38933398b075defde
AFAQ HUSSAIN <161577646+afaqbhutta@users.noreply.github.com>
2025-06-27 19:35:24 +0000

    first change in code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure Flask!"
+    return "Hello, Azure!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
